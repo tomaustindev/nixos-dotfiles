dc55773b0f0a3f1551ace166e930bd051cf38e40
tomaustindev <tomaustindev@gmail.com>
2026-06-08 22:41:39 +0100

    SSH setup.

--- tomthinkcentre/thomas.nix
+++ tomthinkcentre/thomas.nix
@@ -5,7 +5,47 @@
   home.homeDirectory = "/home/thomas";
   home.stateVersion = "25.11";
 
-  programs.neovim.enable = true;
+
+  # Bash
+  # =====
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#tomthinkcentre";
+    };
+
+    initExtra = ''
+      export PS1='[\[\e[91m\]\T\[\e[0m\]]: \[\e[36m\]\u\[\e[0m\] in \[\e[94m\]\w\[\e[0m\] \\$ '
+      export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"
+    '';
+  };
+
+
+  # Neovim
+  # =======
+  programs.neovim = {
+    enable = true;
+    withRuby = false;
+    withPython3 = false;
+  };
+
+
+  # SSH
+  # ====
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      "github.com" = {
+        user = "git";
+        identityFile = "~/.ssh/tomaustindev";
+        identitiesOnly = true;
+        addKeysToAgent = "yes";
+      };
+    };
+  };
+  services.ssh-agent.enable = true;
+
 
   home.packages = with pkgs; [
    # Coding
