6f3f5588a42a14254c2ef051e7c4e638e4e1cdb4
tomaustindev <tomaustindev@gmail.com>
2026-06-08 20:25:28 +0100

    Updated to unstable.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS setup for tomaustindev.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
